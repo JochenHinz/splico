0a31f4374f675b5ed0db4d12f0b34f26aa90e1b7
Fabio Marcinno <fabio.marcinno@epfl.ch>
2024-09-30 12:22:46 +0100

    structure of test flies for mesh quality

---
+++ test/test_qual.py
@@ -0,0 +1,21 @@
+import unittest
+
+import numpy as np
+
+class TestMeshQualityCriteria(unittest.TestCase):
+    
+    def Test_scaled_jacobian(self):
+        ... # TODO: Implement sj test
+    
+    def Test_skewness(self):
+        ... # TODO: Implement skewness test
+         
+    def Test_aspect_ratio(self):
+        ... # TODO: Implement aspect ratio test
+    
+    def Test_orthogonal_quality(self):
+        ... # TODO: Implement ortogonal quality test
+    
+
+if __name__ == '__main__':
+  unittest.main()
