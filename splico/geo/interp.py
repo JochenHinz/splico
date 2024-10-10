a675bd5f7ce46913b7d319b2ecdeb05879221e02
Jochen Hinz <jochen@adminosxs-MacBook-Pro.local>
2024-10-10 20:46:57 +0200

    Add linear spline interpolation.

--- splico/geo/interp.py
+++ splico/geo/interp.py
@@ -2,13 +2,50 @@ from splico.spl import NDSpline, UnivariateKnotVector, TensorKnotVector
 from ..util import np, _
 
 
+def linear_interpolation(spl0: NDSpline, spl1: NDSpline, zdegree: int = 1) -> NDSpline:
+  """
+    Perform a linear interpolation between two `splico.spl.NDSpline`s.
+    This introduces a new parametric dependency which is appended at the end,
+    i.e., spl(x0, x1, ..., xn) -> spl(x0, x1, ..., xn, xn+1).
+
+    Parameters
+    ----------
+    spl0 : :class:`splico.spl.NDSpline`
+        The first input spline.
+    spl1 : :class:`splico.spl.NDSpline`
+        The second input spline.
+    zdegree : :class:`int`
+        The spline basis order in the xn+1-direction. Must be >= 3.
+
+    Returns
+    -------
+    spl : :class:`splico.spl.NDSpline`
+        The resulting linearly interpolated spline satisfying
+        `spl.nvars == spl0.nvars + 1`.
+  """
+  assert spl0.knotvector == spl1.knotvector and spl0.shape == spl1.shape
+  assert (zdegree := int(zdegree)) >= 1
+
+  # make an at least linear knotvector without interior knots
+  kvz = TensorKnotVector([UnivariateKnotVector([0, 1], zdegree)])
+
+  # get the greville points
+  z, = kvz.greville
+
+  # we compute the hermite interpolation functions by fitting againts the
+  # stable greville points. We need not stabilize and the fit should be exact.
+  f0, f1 = kvz.fit([z], np.stack([1 - z, z], axis=1), lam0=0)
+
+  return spl0 * f0 + spl1 * f1
+
+
 def cubic_hermite_interpolation(spl0: NDSpline,
                                 spl1: NDSpline,
                                 t0: np.ndarray,
                                 t1: np.ndarray,
                                 zdegree: int = 3) -> NDSpline:
   """
-    Perform cubic hermite interpolation between two `splico.spl.NDSpline`s.
+    Perform a cubic hermite interpolation between two `splico.spl.NDSpline`s.
     This introduces a new parametric dependency which is appended at the end,
     i.e., spl(x0, x1, ..., xn) -> spl(x0, x1, ..., xn, xn+1).
 
@@ -36,14 +73,11 @@ def cubic_hermite_interpolation(spl0: NDSpline,
       NotImplementedError
   assert (zdegree := int(zdegree)) >= 3
 
-  # make a cubic knotvector without interior knots
+  # make an at least cubic knotvector without interior knots
   kvz = TensorKnotVector([UnivariateKnotVector([0, 1], zdegree)])
-
-  # get the greville points
   z, = kvz.greville
 
-  # we compute the hermite interpolation functions by fitting againts the
-  # stable greville points. We need not stabilize and the fit should be exact.
+  # as in `linear_interpolation`
   f0, f1, f2, f3 = kvz.fit([z], np.stack([2 * z**3 - 3 * z**2 + 1,
                                           z**3 - 2 * z**2 + z,
                                           -2 * z**3 + 3 * z**2,
--- test/test_interp.py
+++ test/test_interp.py
@@ -1,5 +1,5 @@
 from splico.geo import CrossSectionMaker
-from splico.geo.interp import cubic_hermite_interpolation
+from splico.geo.interp import linear_interpolation, cubic_hermite_interpolation
 from splico.mesh import rectilinear, mesh_boundary_union
 from splico.util import np
 
@@ -8,21 +8,25 @@ import unittest
 
 class TestInterpolation(unittest.TestCase):
 
-  def test_cubic_hermite(self):
+  def test_interpolate(self):
     maker = CrossSectionMaker(4)
 
     spl0 = maker.make_disc(.5, .5, 0)
     spl1 = maker.make_disc(2, 2, 0) + np.array([0, 0, 3])[None]
 
-    test = cubic_hermite_interpolation(spl0, spl1, np.array([0, 0, 3]), np.array([0, 0, 3]))
-
     sample_mesh = rectilinear([np.linspace(0, 1, 8),
                                np.linspace(0, 1, 8),
                                np.linspace(0, 1, 101)])
 
-    mesh = mesh_boundary_union(*(mymesh.sample_mesh(sample_mesh) for mymesh in test))
+    with self.subTest('Cubic Hermite interpolation.'):
+      test = linear_interpolation(spl0, spl1)
+      mesh = mesh_boundary_union(*(mymesh.sample_mesh(sample_mesh) for mymesh in test))
+      mesh.plot()
 
-    mesh.plot()
+    with self.subTest('Cubic Hermite interpolation.'):
+      test = cubic_hermite_interpolation(spl0, spl1, np.array([0, 0, 3]), np.array([0, 0, 3]))
+      mesh = mesh_boundary_union(*(mymesh.sample_mesh(sample_mesh) for mymesh in test))
+      mesh.plot()
 
 
 if __name__ == '__main__':
