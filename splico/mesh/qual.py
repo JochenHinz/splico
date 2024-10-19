4c83047cc2192236925bd925ce3fa90fc2c23e44
Fabio Marcinno <fabio.marcinno@epfl.ch>
2024-10-19 10:59:36 +0100

    Further style improvements

--- splico/mesh/qual.py
+++ splico/mesh/qual.py
@@ -12,48 +12,47 @@ from typing import Tuple
 
 IMPLEMENTED_MESH_TYPES = ('triangle', 'tetrahedron', 'quadrilateral', 'hexahedron')
 
- 
+
 def aspect_ratio(mesh: Mesh) -> Tuple[np.float_, ...]:
   """
   Compute the aspect ratio of edges for each element in a
   given mesh.
 
-  This function calculates the aspect ratio (AR) for each element
-  in the mesh, defined as the ratio between the longest and shortest
-  edge in that element. The aspect ratio is a measure of mesh quality:
-  - AR close to 1 indicates a well-proportioned, high-quality mesh 
-     element
-  - AR much greater than 1 indicates a stretched mesh element
+  This function calculates the aspect ratio (AR) for each
+  element in the mesh, defined as the ratio between the
+  longest and shortest edge in that element. The aspect
+  ratio is a measure of mesh quality:
+  - AR close to 1 indicates a well-proportioned,
+    high-quality mesh element
+  - AR much greater than 1 indicates a stretched
+    mesh element
 
   Args:
-   mesh (Mesh): Mesh object (both 2D/3D unstructured and structured)
- 
+   mesh (Mesh): Mesh object (both 2D/3D unstruct/struct)
+
   Returns:
    np.ndarray: mean, maximum and minimum aspect ratio.
 
   Notes:
    - Edge lengths are typically computed using Euclidean distance.
-   - For 2D elements (e.g., triangles, quads), the aspect ratio is based 
-     on edge lengths.
-   - For 3D elements the aspect ratio is still based on edge lengths,
-     but excludes face diagonals or body diagonals.
-      
-  """      
-   
-    # Check for the mesh validity
+   - For 2D elements (e.g., triangles, quads), the aspect
+     ratio is based on edge lengths.
+   - For 3D elements the aspect ratio is still based on
+     edge lengths, but excludes face diagonals or body diagonals.
+  """
   assert mesh.is_valid(), "mesh is not valid"
-  assert mesh.ndims >= 2, "mesh has to be, at least, 2-dimensional entity"
+  assert mesh.ndims >= 2, "mesh has to be, at least, a 2-dimensional entity"
 
-  submesh = mesh.submesh   
-   
+  submesh = mesh.submesh
+
   elements = mesh.elements[:, mesh._submesh_indices]
-   
+
   # Decomposing the mesh
   if mesh._submesh_type is LineMesh: # 2D mesh case
      pass
   else: # 3D mesh case
      elements = elements[:, :, submesh._submesh_indices]
-   
+
   elem_point = mesh.points[elements]
 
   distances = linalg.norm(elem_point[..., 1, :] - elem_point[..., 0, :], axis = -1)
@@ -61,11 +60,9 @@ def aspect_ratio(mesh: Mesh) -> Tuple[np.float_, ...]:
 
   dist_min = np.min(distances, axis = 1)
   dist_max = np.max(distances, axis = 1)
-   
-  # Calculate aspect ratios for each element
+
   aspect_ratios = dist_max / dist_min
 
-  # Making a tuple
   stats = np.mean(aspect_ratios), np.max(aspect_ratios), np.min(aspect_ratios)
 
   return stats
--- splico/mesh/tetgen.py
+++ splico/mesh/tetgen.py
@@ -1,15 +1,19 @@
-from ..util import  _, frozen,  frozen_cached_property
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+@author: Fabio Marcinno'
+"""
+
+from ..util import frozen, frozen_cached_property
 from functools import cached_property
-
 from mesh import Triangulation, AffineMesh
 
 
-
 class TetGen(AffineMesh):
-      
-    '''   
-        
+
+  '''
+
           1
           |\
           |  \
@@ -17,54 +21,36 @@ class TetGen(AffineMesh):
           |      \
           |        \
         0 |__________\ 2
-          /         / 
-         /        /   
-        /       /     
-       /     /        
-      /   /           
-     / /              
-    3                 
+          /         /
+         /        /
+        /       /
+       /     /
+      /   /
+     / /
+    3
 
     the edge between vertices 3-1 is understood
-    '''
-    
-    simplex_type = 'tetrahedron'
-    ndims = 3
-    nverts = 4
-    nref = 4
-    
-    # why _ in front of the function
-    @cached_property
-    def _submesh_indices(self):
-        return tuple(map(frozen, [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]] ))        
+  '''
 
+  simplex_type = 'tetrahedron'
+  ndims = 3
+  nverts = 4
+  nref = 4
 
-    @frozen_cached_property
-    def pvelements(self):
-        return self.elements[:, [0, 3, 1, 2]]
+  @cached_property
+  def _submesh_indices(self):
+    return tuple(map(frozen, [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]]))
 
-    @property
-    def _submesh_type(self):   
-        return Triangulation
-    
-    def subdivide_elements(self):
-       raise NotImplementedError
-     
-    def _refine(self):
-       raise NotImplementedError 
-    
-    
-      
+  @frozen_cached_property
+  def pvelements(self):
+    return self.elements[:, [0, 3, 1, 2]]
 
+  @property
+  def _submesh_type(self):
+    return Triangulation
 
+  def subdivide_elements(self):
+    raise NotImplementedError
 
-
-
-
-
-
-
-
-
-
-
+  def _refine(self):
+    raise NotImplementedError
